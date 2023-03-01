4c667c32d7f550edb783c7303e564e9496c999b8
Martin Ruthaivilavan <mmcruthai@gmail.com>
2023-02-28 22:27:42 -0600

    second commit soccer fantasy

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo
 
 class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me FOREVER')
+    submit = SubmitField('Login')
+
+class PlayerForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    team = StringField('Team', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    nationality = StringField('Nationality', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,47 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm, PlayerForm, BlogForm
 
 @app.route('/')
 def index():
     cdn={
-        'welcome_to_wild_app':"Let's get started!",
-        'meet_our_team': ['Ron', 'Mike', 'Jim', 'Jane']
+        'soccer_fantasy':("Kick-off!"),
+        'top_player': ['John Drinkwater', 'Pierce Hammerstone', 'Paul Debronson', 'Jane McGuoo']
     }
     return render_template('index.jinja', cdn=cdn, title='Home')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.jinja', title='Login')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash (f'Request to login {form.password}!', 'successful')
+        return redirect('/')
+    return render_template('login.jinja', login=form, title='Login')
 
-@app.route('/register')
+@app.route('/register',  methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        flash (f'Request to register {form.username}!', 'successful')
+        return redirect('/')
     return render_template('register.jinja', form=form, title='Register')
 
 @app.route('/about')
 def about():
     return render_template('about.jinja', title='About')
 
-@app.route('/blog')
+@app.route('/blog', methods=['GET', 'POST'])
 def blog():
-    return render_template('blog.jinja', title='Blog')
+    form = BlogForm()
+    if form.validate_on_submit():
+        flash (f'Request to submit post {form.submit}!', 'successful')
+        return redirect('/')
+    return render_template('blog.jinja', blog=form, title='Blog')
+
+@app.route('/player', methods=['GET', 'POST'])
+def player():
+    form = PlayerForm()
+    if form.validate_on_submit():
+        flash (f'Player added {form.submit}!', 'successfully')
+        return redirect('/')
+    return render_template('player.jinja', player=form, title='Player')
